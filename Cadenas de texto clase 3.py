9fbc4311e09f3f0e9914fa1de47d0bc7010b7293
Adsoog <iadso.ao@gmail.com>
2021-04-23 20:21:01 -0500

    Cadenas de texto

---
+++ Cadenas de texto clase 3.py
@@ -0,0 +1,10 @@
+#Operaciones con variables
+a = 14
+b = 12
+c = a + b
+print (a, "hola", b, '=', c)
+
+#A la hora de concatenar numero y cadenas de texo es necesario convertir numeros a variables de texto
+#print (a + "hola" + b + '=' + str(c)) error
+
+print (str(a) + "hola" + str(b) + '=' + str(c))
